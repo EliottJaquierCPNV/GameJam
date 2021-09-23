924d632e6babde73f0e2d9cc8a790f87ef85e1da
Neliott <eliott.jaquier@cpnv.ch>
2021-09-23 23:11:55 +0200

    Bug fixes + minify

--- Assets/Mod Assets/Mod Resources/Scripts/Platformer/PlatformerSpeedPad.cs
+++ Assets/Mod Assets/Mod Resources/Scripts/Platformer/PlatformerSpeedPad.cs
@@ -9,15 +9,19 @@ public class PlatformerSpeedPad : Interactable
     [Range (0, 5)]
     public float duration = 1f;
 
+    private bool isActivated = false;
+
     protected override void OnPlayerInteract(PlayerController player)
     {
-        if(player.GetComponent<Rigidbody2D>() != null)
+        if(player.GetComponent<Rigidbody2D>() != null && !isActivated)
         player.StartCoroutine(PlayerModifier(player, duration));
     }
     IEnumerator PlayerModifier(PlayerController player, float lifetime){
         var initialSpeed = player.maxSpeed;
+        isActivated = true;
         player.maxSpeed = maxSpeed;
         yield return new WaitForSeconds(lifetime);
         player.maxSpeed = initialSpeed;
+        isActivated = false;
     }
 }
